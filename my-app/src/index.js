37e322d7d1422f8d2d8484045de0390f6f6aff86
Stagiaire-Noureddine <noureddine.rakmani@gmail.com>
2023-02-21 14:34:44 +0100

    Added Context to the project
    There's currently only one Context file, although not as organised, it seems an easier way to grasp the concept of it

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Scriptorium from './Scriptorium/Scriptorium';
+import GlobalContext from './GlobalContext';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
+    <GlobalContext>
     <Scriptorium />
+    </GlobalContext>
 );
