fe9768504f291226404c87b16b5561b0da04c2b6
Manon-PM <manon.pms@gmail.com>
2023-02-25 00:34:22 +0100

    Functionnal stats state + dynamic SwiperSlide/Recommended stats/Class description, picture and name on slide change

--- my-app/src/Class/Class.js
+++ my-app/src/Class/Class.js
@@ -21,18 +21,15 @@ const Class = () => {
     // const [equipment, setEquipment] = useState([]);
     const [classes, setClasses] = useState([]);
     const [classesStats, setClassesStats] = useState([]);
-    // const [recommendedStats, setRecommendedStats] = useState([]);
-
-    // let classesStats = {
-    //     class: {
-    //         stat: "",
-    //         isRecommended: ""
-    //     }
-    // };
+    const [selectedClass, setSelectedClass] = useState(0);
 
     const handleToggleEquipment = () => {
         setEquipmentModal(!equipmentModal);
     };
+    
+    const handleSelectClass = (index) => {
+        setSelectedClass(index);
+    }
 
     useEffect(() => {
         axios.get("http://localhost:8080/api/classes")
@@ -42,10 +39,13 @@ const Class = () => {
             setClasses(classData);
             const statsData = classData.map((classObject) => ( {
                 class: classObject.name,
-                stat: Object.keys(classObject.stats),
-                value: Object.values(classObject.stats)
+                stat: Object.entries(classObject.stats).map((stat) => ({
+                    name: stat[0],
+                    isRecommended: stat[1]
+                })),
             }));
             setClassesStats(statsData);
+            // setSelectedClass(0);
         })
         .catch((error) => {
             alert("Erreur API : Les données des classes n'ont pas pu être récupérées.");
@@ -58,6 +58,7 @@ const Class = () => {
     }, [classesStats]);
 
     return (
+        
         <div className="class-container">
             <h1 className="class-title">
                 Classes
@@ -79,34 +80,19 @@ const Class = () => {
                     </div>
                 )}
                 <img className="class-img" src="https://fakeimg.pl/1000x800/EFC874/" alt="Classe" />
-                <div className="class-stat">
-                    {/* {recommendedStats.map((stat) => (
-                        <div className="class-stat-name" key={stat.key}>
-                                {stat.key} {stat.value}
-                        </div>
-                        ))} */}
-                    
-                        {/* <div className="class-stat-name recommended">
-                        Dex
-                    </div>
-                    <div className="class-stat-name">
-                        Con
-                    </div>
-                    <div className="class-stat-name">
-                        Int
-                    </div>
-                    <div className="class-stat-name">
-                        Sag
-                    </div>
-                    <div className="class-stat-name recommended">
-                        Cha
-                    </div> */}
+                <div className="class-stat">           
+                    {classesStats[selectedClass].stat.map((statObj, index) => (
+                        <div className={statObj.isRecommended ? "class-stat-name recommended" : "class-stat-name"}
+                            key={index}>
+                                {statObj.name.substr(0, 3)}
+                        </div>  
+                    ))}
                 </div>
             </div>
 
             <div className="class-description">
                 <p className="class-description-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl.
+                    {classes[selectedClass].description}
                 </p>
             </div>
 
@@ -117,38 +103,16 @@ const Class = () => {
                 mousewheel={true}
                 centeredSlides={true}
                 slidesPerView={3}
-                spaceBetween={3}>
-                {/*Map le tableau des classes pour générer les SwiperSlide*/}
-                    <SwiperSlide>
+                spaceBetween={3}
+                onSlideChange={(swiper) => {handleSelectClass(swiper.realIndex)}}>
+                {classes.map((classObj) => (
+                    <SwiperSlide key={classObj.id}>
                         <div className="class-carrousel-item">
-                            <img className="class-carrousel-img" src="https://fakeimg.pl/90x90/000/" alt="Classe" />
+                            <img className="class-carrousel-img" src={classObj.picture} alt="Classe" />
+                            <p>{classObj.name} </p>
                         </div>
                     </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="class-carrousel-item">
-                            <img className="class-carrousel-img" src="https://fakeimg.pl/90x90/000/" alt="Classe" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="class-carrousel-item">
-                            <img className="class-carrousel-img" src="https://fakeimg.pl/90x90/000/" alt="Classe" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="class-carrousel-item">
-                            <img className="class-carrousel-img" src="https://fakeimg.pl/90x90/000/" alt="Classe" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="class-carrousel-item">
-                            <img className="class-carrousel-img" src="https://fakeimg.pl/90x90/000/" alt="Classe" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="class-carrousel-item">
-                            <img className="class-carrousel-img" src="https://fakeimg.pl/90x90/000/" alt="Classe" />
-                        </div>
-                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
     );
