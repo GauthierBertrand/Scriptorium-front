515d36a8205f348132d9e3ed8fbc071c05e5afb6
Stagiaire-Noureddine <noureddine.rakmani@gmail.com>
2023-02-26 02:16:33 +0100

    fixed routes again

--- my-app/src/Scriptorium/Scriptorium.js
+++ my-app/src/Scriptorium/Scriptorium.js
@@ -15,36 +15,36 @@ import { Routes, Route } from "react-router-dom";
 
 const Scriptorium = () => {
   return (
-  <Routes>
     <div>
-      {/* <User /> */}
-      {/* <Route path="/classe" element={<Class/>} /> */}
-      {/* {races.map((race, index) => (
-        <Route 
-          key={index}
-          path="/general" 
-          element={(props) => (
-            <Race
-              {...props}
-              name={race.name}
-              description={race.description}
-              bonus1={race.bonus1}
-              bonus2={race.bonus2}
-            />
-          )}
-        />
-      ))} */}
-    {/* <Route path="/general" element={<General/>} />
-        <Route path="/generation-des-stats" element={<Generator/>} />
-        <Route path="/stats" element={<Stat/>} />
-        <Route path="/voies" element={<Way/>} />
-        <Route path="/apercu" element={<Preview/>} />
-        <Route path="/mentions-legales" element={<Notice/>} />
-        <Route path="/fiches" element={<Sheet/>} />
-        <Route path="/profil" element={<Profile/>} /> */}
-      {/* <Register /> */}
+      <Routes>
+        {/* <User /> */}
+        {/* <Route path="/classe" element={<Class/>} /> */}
+        {/* {races.map((race, index) => (
+          <Route 
+            key={index}
+            path="/general" 
+            element={(props) => (
+              <Race
+                {...props}
+                name={race.name}
+                description={race.description}
+                bonus1={race.bonus1}
+                bonus2={race.bonus2}
+              />
+            )}
+          />
+        ))} */}
+      {/* <Route path="/general" element={<General/>} />
+          <Route path="/generation-des-stats" element={<Generator/>} />
+          <Route path="/stats" element={<Stat/>} />
+          <Route path="/voies" element={<Way/>} />
+          <Route path="/apercu" element={<Preview/>} />
+          <Route path="/mentions-legales" element={<Notice/>} />
+          <Route path="/fiches" element={<Sheet/>} />
+          <Route path="/profil" element={<Profile/>} /> */}
+        {/* <Register /> */}
+      </Routes>
     </div>
-  </Routes>
   );
 };
 
