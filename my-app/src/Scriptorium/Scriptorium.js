a1ea03d72914b9d36ccef6f91ae5f44c3d9b95d4
Manon-PM <manon.pms@gmail.com>
2023-02-26 12:26:40 +0100

    Router imported from component-scriptorium branch

--- my-app/src/Scriptorium/Scriptorium.js
+++ my-app/src/Scriptorium/Scriptorium.js
@@ -5,27 +5,27 @@ import Class from "../Class/Class";
 // import Stat from "../Stat/Stat";
 // import Way from "../Way/Way";
 // import Preview from "../Preview/Preview";
-import Navbar from "../User/Navbar";
+// import Navbar from "../User/Navbar";
 // import Notice from "../Notice/Notice";
 // import Sheet from "../Sheet/Sheet";
-import Profile from "../Profile/Profile";
-import Settings from "../User/Settings";
+// import Profile from "../Profile/Profile";
+// import Settings from "../User/Settings";
 // import Register from "../Register/Register";
-import ProtectedRoute from "../User/ProtectedRoute";
+// import ProtectedRoute from "../User/ProtectedRoute";
 import { Routes, Route } from "react-router-dom";
 import { UserProvider } from "../UserContext";
 
 const Scriptorium = () => {
   return (
     <div>
-      <UserProvider>
-          <Navbar />
+        <UserProvider>
+          {/* <Navbar /> */}
         </UserProvider>
         <Routes>
-          <Route path="/profile" component={ProtectedRoute(Profile)} />
-          <Route path="/parametres" component={ProtectedRoute(Settings)} />
+          {/* <Route path="/profile" component={ProtectedRoute(Profile)} /> */}
+          {/* <Route path="/parametres" component={ProtectedRoute(Settings)} /> */}
         {/* <Route path="/mentions-legales" component={(MentionsLegales)} /> */}
-        {/* <Route path="/classe" element={<Class/>} /> */}
+        <Route path="/classe" element={<Class/>} />
         {/* {races.map((race, index) => (
           <Route 
             key={index}
