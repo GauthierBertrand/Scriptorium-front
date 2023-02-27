0b46f3b79d3ac1a2cd0b1efe2c2610fe2f7d847f
Manon-PM <manon.pms@gmail.com>
2023-02-27 18:16:54 +0100

    Starting to make items dynamic

--- my-app/src/Way/Way.js
+++ my-app/src/Way/Way.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import SwiperCore, { Navigation, Keyboard, Mousewheel } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+
+import axios from "axios";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -13,53 +15,65 @@ import "./Way.scss";
 
 SwiperCore.use([ Navigation, Keyboard, Mousewheel ]);
 
-const Way = ({ stats, otherStats, points }) => {
+const Way = () => {
 
     const [descriptionOpen, setDescriptionOpen] = useState(false);
+    const [ways, setWays] = useState([]);
 
     const handleToggleDescription = () => {
         setDescriptionOpen(!descriptionOpen);
     }
 
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/ways")
+        .then((response) => {
+            const waysData = response.data.ways;
+            console.log(waysData);
+            setWays(waysData);
+        })
+        .catch((error) => {
+            alert("Erreur API : Les données des voies n'ont pas pu être récupérées.");
+            console.error(error);
+        })
+    }, []);
+
     return (
         <>
         <div className="stats-header">
-        <div className="stats-header-mod">
-            {stats.map((stat, index) => (
-                <div className="stat-info" key={index}>
-                    {stat.name}
-                    <div className="stat-mod">{stat.mod}</div>
+            <div className="stats-header-mod">
+                <div className="stat-info">
+                    For
+                    <div className="stat-mod">+2</div>
                 </div>
-            ))}
             </div>
             <div className="stats-header-other">
                 <div className="stat-atk">
                     <div className="stat-type atk">
-                        <img src="#" alt="Type d'attaque" />
+                        <img src="https://fakeimg.pl/16/000" alt="Type d'attaque" />
                         <div className="stat-type-value">12</div>
                     </div>
                     <div className="stat-type atk">
-                        <img src="#" alt="Type d'attaque" />
+                        <img src="https://fakeimg.pl/16/000" alt="Type d'attaque" />
                         <div className="stat-type-value">12</div>
                     </div>
                     <div className="stat-type atk">
-                        <img src="#" alt="Type d'attaque" />
+                        <img src="https://fakeimg.pl/16/000" alt="Type d'attaque" />
                         <div className="stat-type-value">12</div>
                     </div>
                 </div>
                 <div className="stat-def">
                     <div className="stat-type def">
-                        <img src="#" alt="Type de défense" />
+                        <img src="https://fakeimg.pl/16/000" alt="Type de défense" />
                         <div className="stat-type-value">12</div>
                     </div>
                     <div className="stat-type def">
-                        <img src="#" alt="Type de défense" />
+                        <img src="https://fakeimg.pl/16/000" alt="Type de défense" />
                         <div className="stat-type-value">12</div>
                     </div>
                 </div>
             </div>
             <div className="way-points">
-                <button className="remaining-points">{points}</button>
+                <button className="remaining-points">3</button>
                 <div className="remaining-points-text">points disponibles</div>
             </div>
         </div>
@@ -68,93 +82,66 @@ const Way = ({ stats, otherStats, points }) => {
             loop={true}
             navigation={false}
             keyboard={true}
-            mousewheel={true}>
-            <SwiperSlide>
-                <div className="way-container">
-                    <div className="way-name" onClick={handleToggleDescription}>
-                        Nom de la voie
+            mousewheel={false}>
+            {ways.map((way) => (
+                <SwiperSlide key={way.id}>
+                    <div className="way-container">
+                        <div className="way-name" onClick={handleToggleDescription}>
+                            {way.name}
+                        </div>
+                            <button className={`race ${descriptionOpen ? "way-button hidden" : "way-button"}`}
+                                    onClick={handleToggleDescription}>
+                                        &#9207;
+                            </button>
+                        {descriptionOpen && (
+                            way.wayAbilities.map((wayAbility, index) => (
+                                
+                              <div className="way-ability-container" key={index}>
+                                {console.log(wayAbility.bonus)}
+                               <div className="way-ability">
+                                    <div className="way-ability-name">
+                                        {wayAbility.name}
+                                    </div>
+                                    <div className="way-ability-description">
+                                        {wayAbility.description}
+                                    </div>
+                                    <div className="way-ability-bonus">
+                                        {/* {wayAbility.bonus.FOR && wayAbility.bonus.DEX && wayAbility.bonus.CON && wayAbility.bonus.INT && wayAbility.bonus.SAG && wayAbility.bonus.CHA && wayAbility.bonus.INIT && wayAbility.bonus.DEF && wayAbility.bonus.PV && (
+                                                wayAbility.bonus.FOR,
+                                                wayAbility.bonus.DEX,
+                                                wayAbility.bonus.CON,
+                                                wayAbility.bonus.INT,
+                                                wayAbility.bonus.SAG,
+                                                wayAbility.bonus.CHA,
+                                                wayAbility.bonus.INIT,
+                                                wayAbility.bonus.DEF,
+                                                wayAbility.bonus.PV
+                                        )} */}
+                                    </div>
+                                </div>
+                                <button className="way-button open"
+                                    onClick={handleToggleDescription}>
+                                        &#9207;
+                                </button> 
+                            </div>
+                            ))                            
+                        )}
+                        {!descriptionOpen && (
+                            <div className="way-changes-container">
+                                <h3 className="changes-summary">
+                                    Résumé des changements :
+                                </h3>
+                                <div className="way-changes">
+                                    <img src="#" alt="Logo décoratif des changements"/>
+                                    <p className="feature-changing">
+                                        Discrétion +5
+                                    </p>
+                                </div>
+                            </div>
+                        )}
                     </div>
-                        <button className={`race ${descriptionOpen ? "way-button hidden" : "way-button"}`}
-                                onClick={handleToggleDescription}>
-                                    &#9207;
-                        </button>
-                    {descriptionOpen && (
-                        <>
-                        <div className="way-ability">
-                            <div className="way-ability-name">
-                                Nom de la compétence de voie
-                            </div>
-                            <div className="way-ability-description">
-                                Description de la compétence de voie
-                            </div>
-                            <div className="way-ability-bonus">
-                                Bonus de la compétence de voie
-                            </div>
-                        </div>
-                        <button className="way-button open"
-                            onClick={handleToggleDescription}>
-                                &#9207;
-                        </button>
-                        </>
-                    )}
-                    {!descriptionOpen && (
-                        <div className="way-changes-container">
-                            <h3 className="changes-summary">
-                                Résumé des changements :
-                            </h3>
-                            <div className="way-changes">
-                                <img src="#" alt="Logo décoratif des changements"/>
-                                <p className="feature-changing">
-                                    Discrétion +5
-                                </p>
-                            </div>
-                        </div>
-                    )}
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="way-container">
-                    <div className="way-name" onClick={handleToggleDescription}>
-                        Nom de la voie
-                    </div>
-                        <button className={`race ${descriptionOpen ? "way-button hidden" : "way-button"}`}
-                                onClick={handleToggleDescription}>
-                                    &#9207;
-                        </button>
-                    {descriptionOpen && (
-                        <>
-                        <div className="way-ability">
-                            <div className="way-ability-name">
-                                Nom de la compétence de voie
-                            </div>
-                            <div className="way-ability-description">
-                                Description de la compétence de voie
-                            </div>
-                            <div className="way-ability-bonus">
-                                Bonus de la compétence de voie
-                            </div>
-                        </div>
-                        <button className="way-button open"
-                            onClick={handleToggleDescription}>
-                                &#9207;
-                        </button>
-                        </>
-                    )}
-                    {!descriptionOpen && (
-                        <div className="way-changes-container">
-                            <h3 className="changes-summary">
-                                Résumé des changements :
-                            </h3>
-                            <div className="way-changes">
-                                <img src="#" alt="Logo décoratif des changements"/>
-                                <p className="feature-changing">
-                                    Discrétion +5
-                                </p>
-                            </div>
-                        </div>
-                    )}
-                </div>
-            </SwiperSlide>
+                </SwiperSlide>
+            ))}
         </Swiper>
     </>
     );
