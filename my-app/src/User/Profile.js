a26e3f3e23602455a5959710703851670068b3d4
Manon-PM <manon.pms@gmail.com>
2023-03-07 14:32:17 +0100

    User sheets OK + a few CSS

--- my-app/src/User/Profile.js
+++ my-app/src/User/Profile.js
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Cookies from 'js-cookie';
 import { UserContext } from '../UserContext';
 import { useEffect } from 'react';
 import axios from 'axios';
+import './Profile.scss';
 
 
 const Profile = () => {
@@ -13,14 +14,17 @@ const Profile = () => {
 
     const token = Cookies.get('token');
 
+    const [sheetsList, setSheetsList] = useState([]);
+
   useEffect (() => {
     axios.get("http://localhost:8080/api/characters/users", {
       headers: {
         "Authorization":  `Bearer ${token}`
       }})
     .then((response) => {
-      const sheetsData = response.data;
+      const sheetsData = response.data.sheets;
       console.log(sheetsData);
+      setSheetsList(sheetsData);
     })
   }, []);
 
@@ -30,14 +34,41 @@ const Profile = () => {
     navigate('/classes');
   };
 
-  return (
+  return ( 
     <div className="sheet-container">
+    <button onClick={handleLogout}>Logout</button>
       <h1>Mes fiches enregistrées</h1>
       <div className="sheet-list">
-        <div className="sheet">
-        </div>
+        {sheetsList.map((sheet, index) => (
+          <div className="sheet" key={index}>
+            <div className="sheet-infos">
+              <div className="sheet-picture">
+                <img className="character-picture" src={sheet.picture} alt="Portrait du personnage" height={80} /> {/* height to disable once the picture is really dynamic*/}
+              </div>
+              <div className="sheet-name">
+                {sheet.character_name}
+              </div>
+              <div className="sheet-race">
+                {sheet.race_name}
+              </div>
+              <div className="sheet-class">
+                {sheet.classe.name}
+              </div>
+            </div>
+            <div className="sheet-actions">
+              <button className="sheet-button">
+                <img src="https://fakeimg.pl/20x20/000/?text=DL" alt="Télécharger la fiche" />
+              </button>
+              <button className="sheet-button">
+                <img src="https://fakeimg.pl/20x20/000/?test=EDIT" alt="Modifier la fiche" />
+              </button>
+              <button className="sheet-button">
+                <img src="https://fakeimg.pl/20x20/000/?text=SUPP" alt="Supprimer la fiche" />
+              </button>
+            </div>
+          </div>
+          ))}
       </div>
-      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
