1ed4b2f2fc40b145ac046a455ef0c1c6ba475807
Manon-PM <manon.pms@gmail.com>
2023-03-07 18:42:01 +0100

    Factorise some CSS + vars

--- my-app/src/Class/Class.js
+++ my-app/src/Class/Class.js
@@ -19,6 +19,7 @@ import "./../reset.css";
 import "./Class.scss";
 
 import next from "./../assets/images/next.png";
+import barde from "./../assets/images/barde.png";
 
 SwiperCore.use([ Navigation, Keyboard, Mousewheel ]);
 
@@ -74,9 +75,9 @@ const Class = () => {
 
     return (
         <div className="class-container">
-            <h1 className="class-title">
+            {/* <h1 className="class-title">
                 Classes
-            </h1>
+            </h1> */}
 
             <div className="class-img-container">
                 <button className="equipment-button" onClick={handleToggleEquipment}>
@@ -95,7 +96,7 @@ const Class = () => {
                     ))}
                     </div>
                 )}
-                <img className="class-img" src="https://fakeimg.pl/1000x800/EFC874/" alt="Classe" />
+                <img className="class-img" src={barde} alt="Classe" />
                 {classesStats[selectedClass] && (
                     <div className="class-stat">           
                         {classesStats[selectedClass].stat.map((statObj, index) => (
